9893772fb8d31b72db4d36b199cc51c2053520a5
xm306088569 <306088569@qq.com>
2021-07-14 20:22:02 +0800

    修改

--- src/Facades/Ntnacos.php
+++ src/Facades/Ntnacos.php
@@ -1,10 +1,10 @@
 <?php
     namespace Nt\Nacos\Facades;
     use Illuminate\Support\Facades\Facade;
-    class Nacos extends Facade
+    class Ntnacos extends Facade
     {
         protected static function getFacadeAccessor()
         {
-            return 'nacos';
+            return 'ntnacos';
         }
     }
