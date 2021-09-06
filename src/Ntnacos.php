a49e22a4f390a23477c86acfff01f8fa3b102242
xm306088569 <33388012+xm306088569@users.noreply.github.com>
2021-09-06 10:32:10 +0800

    增加配置项，可以本地关闭

--- src/Ntnacos.php
+++ src/Ntnacos.php
@@ -5,30 +5,31 @@ namespace Nt\Nacos;
 class Ntnacos
 {
 
-        function app_nacos($env)
-        {
-        
-            \alibaba\nacos\NacosConfig::setSnapshotPath(dirname(__DIR__) . "/nacos/config");
-            (new \Dotenv\Loader([], new \Dotenv\Environment\DotenvFactory(), true))->loadDirect(
-                \alibaba\nacos\failover\LocalConfigInfoProcessor::getSnapshot(
-                    $env['NACOS_HOST'] ,
-                    $env['NACOS_DATA_ID'],
-                    $env['NACOS_GROUP'],
-                    "",
-                )
-            );
+       function app_nacos($env)
+    {
+
+        \alibaba\nacos\NacosConfig::setSnapshotPath(dirname(__DIR__) . "/nacos/config");
+        (new \Dotenv\Loader([], new \Dotenv\Environment\DotenvFactory(), true))->loadDirect(
+            \alibaba\nacos\failover\LocalConfigInfoProcessor::getSnapshot(
+                $env['NACOS_HOST'],
+                $env['NACOS_DATA_ID'],
+                $env['NACOS_GROUP'],
+                "",
+            )
+        );
+        if ($env['NACOS_ENV'] != 'local') {
             (new \Dotenv\Loader([], new \Dotenv\Environment\DotenvFactory(), true))->loadDirect(
                 \alibaba\nacos\Nacos::init(
-                    $env['NACOS_HOST'] ,
+                    $env['NACOS_HOST'],
                     $env['NACOS_ENV'],
                     $env['NACOS_DATA_ID'],
                     $env['NACOS_GROUP'],
                     $env['USER_NAME'],
-                    $env['PASSWORD'],    
+                    $env['PASSWORD'],
                     ""
                 )->runOnce()
             );
-
         }
+    }
 
 }
